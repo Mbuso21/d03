4fff34975d6b479bf6b469e280cb003c598b3537
Mbuso21 <newmbuso@gmail.com>
2020-06-24 17:08:40 +0200

    Ex07

--- ex07/ft_strrev.c
+++ ex07/ft_strrev.c
@@ -6,16 +6,58 @@
 /*   By: mmehloma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/24 10:17:22 by mmehloma          #+#    #+#             */
-/*   Updated: 2020/06/24 10:22:13 by mmehloma         ###   ########.fr       */
+/*   Updated: 2020/06/24 15:36:00 by mmehloma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-char *ft_strrev(char *str)
+void ft_strrev(char *str)
 {
-	int a;
+	int i;
+	int x;
 	char *rev;
-	a = 0;
 
+	i = 0;
+	x = 0;
+	while(str[x])
+	{
+		i++;
+	}
+	
+	x = 0;
 
+	while(str[i])
+	{
+		rev[x] = str[i];
+		x++;
+		i--;
+	}
+
+	while(rev[i])
+	{
+		write(1, &rev[i], 1);
+	}
+
+}
+
+int main(void)
+{
+	
+	ft_strrev("Hello");
+	return 0;
+	
+
+/*	char *print;
+	int i;
+
+	i = 0;
+
+	*print = *ft_strrev("Hello");
+
+	while(print[i])
+	{
+		write(1, &print[i], 1);
+		i++;
+	}*/
+}
